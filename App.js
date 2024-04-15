30441a84fed61744871b26ee8f665e0dd76e2a93
NicollasTavares <126300013+NicollasTavares@users.noreply.github.com>
2024-04-15 17:59:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+import Home from './src/pages/Home';
+import Lista from './src/pages/Lista'
+import Cadastro from './src/pages/Cadastro';
+import Pesquisa from './src/pages/Pesquisa';
+import EditarouExcluir from './src/pages/EditarouExcluir';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={Home}
+        />
+        <Stack.Screen
+          name='Lista'
+          component={Lista}
+        />
+        <Stack.Screen
+          name='Cadastrar'
+          component={Cadastro}
+        />
+        <Stack.Screen
+          name='Pesquisar'
+          component={Pesquisa}
+        />
+        <Stack.Screen
+          name='EditarouExcluir'
+          component={EditarouExcluir}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
